f1b666bc4e596cedae38975b0f51bde0c9f7b255
Wayne Krug <wmkrug@gmail.com>
2015-04-09 21:00:51 -0500

    Removed Nim, Crystal, and LLVM D compiler
    I don't feel like messing with those right now.  I might put them back
    later.

--- run.bash
+++ run.bash
@@ -8,8 +8,6 @@ echo -e "\n=== mono C#:"
 perf stat -r 10 ./bin_test_cs 2>&1 > /dev/null | grep time
 echo -e "\n=== D (dmd):"
 perf stat -r 10 ./bin_test_d_dmd 2>&1 > /dev/null | grep time
-echo -e "\n=== D (ldc2):"
-perf stat -r 10 ./bin_test_d_ldc 2>&1 > /dev/null | grep time
 echo -e "\n=== D (gdc):"
 perf stat -r 10 ./bin_test_d_gdc 2>&1 > /dev/null | grep time
 echo -e "\n=== Go gc:"
@@ -18,11 +16,5 @@ echo -e "\n=== Go gccgo -O3:"
 perf stat -r 10 ./bin_test_go_gccgo 2>&1 > /dev/null | grep time
 echo -e "\n=== Rust:"
 perf stat -r 10 ./bin_test_rs 2>&1 >/dev/null | grep time
-echo -e "\n=== Nim (gcc):"
-perf stat -r 10 ./bin_test_nim_gcc 2>&1 > /dev/null | grep time
-echo -e "\n=== Nim (clang):"
-perf stat -r 10 ./bin_test_nim_clang 2>&1 > /dev/null | grep time
-echo -e "\n=== Crystal:"
-perf stat -r 10 ./bin_test_cr 2>&1 > /dev/null | grep time
 echo -e "\n=== Java:"
 perf stat -r 10 java -cp . test 2>&1 > /dev/null | grep time
