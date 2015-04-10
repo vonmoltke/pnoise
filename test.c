0de4a7e14025734d4a236552bad8179149661363
Wayne Krug <wmkrug@gmail.com>
2015-04-09 22:30:27 -0500

    Eliminated get_gradients
    Eliminated get_gradients by manually inlining the code in noise2d_get.
    Also eliminated the two vectors in the noise data struct that were
    previously only used to pass data betweem the functions.  This change
    had the curious effect of speeding up clang by about 30% and slowing
    down gcc by about 30%.  At some point I need to look at the generated
    assembly to see what happened there.

--- test.c
+++ test.c
@@ -7,118 +7,131 @@
 
 #define CUBE_DIM  (1024)
 #define NUM_ITERS  (100)
+#define RAND_SCALE (1.0f / RAND_MAX * M_PI * 2.0f);
 
-typedef struct {
-        float x, y;
+typedef struct
+{
+  float x, y;
 } Vec2;
 
-static inline float lerp(float a, float b, float v)
+static inline float lerp(const float a, const float b, const float v)
 {
-	return a * (1 - v) + b * v;
+  return a * (1 - v) + b * v;
 }
 
-static inline float smooth(float v) {
-	return v * v * (3 - 2 * v);
+static inline float smooth(const float v)
+{
+  return v * v * (3 - 2 * v);
 }
 
 static inline Vec2 random_gradient()
 {
-	const float v = (float)rand() / RAND_MAX * M_PI * 2.0f;
-	return (Vec2){cosf(v), sinf(v)};
+  const float v = (float) rand() * RAND_SCALE;
+  return (Vec2) { cosf (v), sinf (v) };
 }
 
-static inline float gradient(Vec2 orig, Vec2 grad, Vec2 p)
+static inline float gradient(const Vec2 orig, const Vec2 grad, const Vec2 p)
 {
-        Vec2 sp = {p.x - orig.x, p.y - orig.y};
-	return grad.x * sp.x + grad.y * sp.y;
+  return grad.x * (p.x - orig.x) + grad.y * (p.y - orig.y);
 }
 
-typedef struct {
-        Vec2 rgradients[CUBE_DIM];
-        int  permutations[CUBE_DIM];
-        Vec2 gradients[4];
-        Vec2 origins[4];
+typedef struct
+{
+  Vec2 rgradients[CUBE_DIM];
+  int permutations[CUBE_DIM];
 } Noise2DContext;
 
-static inline Vec2 get_gradient(Noise2DContext *ctx, int x, int y) {
-	int idx = ctx->permutations[x & (CUBE_DIM - 1)] + ctx->permutations[y & (CUBE_DIM - 1)];
-	return ctx->rgradients[idx & (CUBE_DIM - 1)];
-}
-
-static inline void get_gradients(Noise2DContext *ctx, float x, float y) {
-	float x0f = floorf(x);
-	float y0f = floorf(y);
-	int x0 = x0f;
-	int y0 = y0f;
-	int x1 = x0 + 1;
-	int y1 = y0 + 1;
-
-	ctx->gradients[0] = get_gradient(ctx, x0, y0);
-	ctx->gradients[1] = get_gradient(ctx, x1, y0);
-	ctx->gradients[2] = get_gradient(ctx, x0, y1);
-	ctx->gradients[3] = get_gradient(ctx, x1, y1);
-
-	ctx->origins[0] = (Vec2){x0f + 0.0f, y0f + 0.0f};
-	ctx->origins[1] = (Vec2){x0f + 1.0f, y0f + 0.0f};
-	ctx->origins[2] = (Vec2){x0f + 0.0f, y0f + 1.0f};
-	ctx->origins[3] = (Vec2){x0f + 1.0f, y0f + 1.0f};
-}
-
-
-static float noise2d_get(Noise2DContext *ctx, float x, float y)
+static inline Vec2 get_gradient(Noise2DContext* const ctx, const int x, const int y)
 {
-	Vec2 p = {x, y};
-	get_gradients(ctx, x, y);
-	float v0 = gradient(ctx->origins[0], ctx->gradients[0], p);
-	float v1 = gradient(ctx->origins[1], ctx->gradients[1], p);
-	float v2 = gradient(ctx->origins[2], ctx->gradients[2], p);
-	float v3 = gradient(ctx->origins[3], ctx->gradients[3], p);
-
-	float fx = smooth(x - ctx->origins[0].x);
-	float vx0 = lerp(v0, v1, fx);
-	float vx1 = lerp(v2, v3, fx);
-	float fy = smooth(y - ctx->origins[0].y);
-        return lerp(vx0, vx1, fy);
+  const int idx = ctx->permutations[x & (CUBE_DIM - 1)] + ctx->permutations[y & (CUBE_DIM - 1)];
+  return ctx->rgradients[idx & (CUBE_DIM - 1)];
 }
 
-static void init_noise2d(Noise2DContext *ctx)
+static float noise2d_get(Noise2DContext* const ctx, const float x, const float y)
 {
-        for (int i = 0; i < CUBE_DIM; i++)
-                ctx->rgradients[i] = random_gradient();
+  Vec2 p = { x, y };
+  
+  const float x0f = floorf(x);
+  const float y0f = floorf(y);
+  const int x0 = x0f;
+  const int y0 = y0f;
+  const int x1 = x0 + 1;
+  const int y1 = y0 + 1;
 
-        for (int i = 0; i < CUBE_DIM; i++) {
-                int j = rand() % (i+1);
-		ctx->permutations[i] = ctx->permutations[j];
-                ctx->permutations[j] = i;
-        }
+  const Vec2 grad0 = get_gradient(ctx, x0, y0);
+  const Vec2 grad1 = get_gradient(ctx, x1, y0);
+  const Vec2 grad2 = get_gradient(ctx, x0, y1);
+  const Vec2 grad3 = get_gradient(ctx, x1, y1);
+
+  const Vec2 origin0 = { x0f, y0f };
+  const Vec2 origin1 = { x0f + 1.0f, y0f };
+  const Vec2 origin2 = { x0f, y0f + 1.0f };
+  const Vec2 origin3 = { x0f + 1.0f, y0f + 1.0f };
+  
+  float v0 = gradient(origin0, grad0, p);
+  float v1 = gradient(origin1, grad1, p);
+  float v2 = gradient(origin2, grad2, p);
+  float v3 = gradient(origin3, grad3, p);
+
+  float fx = smooth(x - origin0.x);
+  float vx0 = lerp(v0, v1, fx);
+  float vx1 = lerp(v2, v3, fx);
+  float fy = smooth(y - origin0.y);
+  return lerp (vx0, vx1, fy);
 }
 
-int main(int argc, char **argv)
+static void init_noise2d(Noise2DContext* const ctx)
 {
-        srand(time(NULL));
+  for (int i = 0; i < CUBE_DIM; i++)
+    ctx->rgradients[i] = random_gradient();
 
-        const char *symbols[] = {" ", "░", "▒", "▓", "█", "█"};
-        float *pixels = malloc(sizeof(float) * CUBE_DIM * CUBE_DIM);
+  for (int i = 0; i < CUBE_DIM; i++)
+  {
+    const int j = rand () % (i + 1);
+    ctx->permutations[i] = ctx->permutations[j];
+    ctx->permutations[j] = i;
+  }
+}
 
-        Noise2DContext n2d;
-	init_noise2d(&n2d);
+int main(const int argc, const char **argv)
+{
+  srand(time (NULL));
 
-        for (int i = 0; i < NUM_ITERS; i++) {
-                for (int y = 0; y < CUBE_DIM; y++) {
-                        for (int x = 0; x < CUBE_DIM; x++) {
-                                float v = noise2d_get(&n2d, x * 0.1f, y * 0.1f)	* 0.5f + 0.5f;
-                                pixels[y * CUBE_DIM + x] = v;
-                        }
-                }
-        }
+  const char *symbols[] = { " ", "░", "▒", "▓", "█", "█" };
+  float *pixels = malloc (sizeof (float) * CUBE_DIM * CUBE_DIM);
 
-        for (int y = 0; y < CUBE_DIM; y++) {
-                for (int x = 0; x < CUBE_DIM; x++) {
-			int idx = pixels[y * CUBE_DIM + x] / 0.2f;
-                        printf("%s", symbols[idx]);
-		}
-                printf("\n");
-        }
+  Noise2DContext n2d;
+  init_noise2d(&n2d);
 
-        return 0;
+  int pixel_index = 0;
+  for (int i = 0; i < NUM_ITERS; ++i)
+  {
+    pixel_index = 0;
+
+    for (int y = 0; y < CUBE_DIM; ++y)
+    {
+      const float y_noise_coord = y * 0.1f;
+
+      for (int x = 0; x < CUBE_DIM; ++x)
+      {
+	const float v = noise2d_get(&n2d, x * 0.1f, y_noise_coord) * 0.5f + 0.5f;
+	pixels[pixel_index] = v;
+        ++pixel_index;
+      }
+    }
+  }
+
+  pixel_index = 0;
+  for (int y = 0; y < CUBE_DIM; ++y)
+  {
+    for (int x = 0; x < CUBE_DIM; ++x)
+    {
+      const int idx = pixels[pixel_index] * 5.0f;
+      printf ("%s", symbols[idx]);
+      ++pixel_index;
+    }
+    printf ("\n");
+  }
+
+  return 0;
 }
